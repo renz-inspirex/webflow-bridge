1429aa92748a5b90281a0560af582673e883a78f
Kenneth <renz@inspirex.io>
2022-08-19 11:10:14 +0800

    testing

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+	console.log({
+		body: JSON.stringify(req.body),
+		params: JSON.stringify(res.query),
+	});
 	res.send("Hello World!");
 });
 
